bb3bc14c95e0d89cb9357aca46e0639295704baf
VISSHVA K 2001 <161825499+visshva@users.noreply.github.com>
2024-05-21 17:41:14 +0530

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
     apiKey: "AIzaSyA-sD_ZEd9xydoIfU74QvgbtLl5mlSZ2Kg",
     authDomain: "iqacdoc.firebaseapp.com",
@@ -14,10 +14,10 @@ const firebaseConfig = {
 };
 const app = initializeApp(firebaseConfig);
 
-const db = getFirestore(app);
+
 
 // Initialize Firebase services
 const storage = getStorage(app);
 const auth = getAuth(app);
 
-export { db,storage, auth };
+export { storage, auth };
